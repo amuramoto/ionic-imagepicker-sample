04895680772e00b5d15724a5a0ea39995142190d
Alex Muramoto <amuramoto@apache.org>
2016-08-05 13:14:20 -0700

    Strips down code to starter project

--- app/pages/gallery/gallery.ts
+++ app/pages/gallery/gallery.ts
@@ -1,36 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/gallery/gallery.html',
 })
 export class GalleryPage {
 
-	private images: Array<string>;	
-	private rows: Array<Array<string>>;
-  
-  constructor(private _navCtrl: NavController, private _navParams: NavParams) {
-  	this.images = this._navParams.get('images');
-  	this.rows = Array(Math.ceil(this.images.length/2));
-  }
-
-  ionViewLoaded() {
-  	
-  	var rowNum = 0;
-  	
-  	for (var i = 0; i < this.images.length; i+=2) {
-  		this.rows[rowNum] = Array(2);
-  		if (this.images[i]) {
-  			this.rows[rowNum][0] = this.images[i]
-  		}
-  		if (this.images[i+1]) {
-  			this.rows[rowNum][1] = this.images[i+1]
-  		}
-  		
-  	
-  		rowNum++;
-  	}
-  		
-  }
+  constructor(private _navCtrl: NavController) {}
 
 }
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
-import { ImagePicker } from 'ionic-native';
-import {GalleryPage} from '../gallery/gallery';
  
 @Component({
   templateUrl: 'build/pages/home/home.html'
@@ -9,23 +7,7 @@ import {GalleryPage} from '../gallery/gallery';
 
 export class HomePage {
 
-  constructor(private _navCtrl: NavController) {
-
-  }
-
-  private openGallery (): void {
-    let options = {
-      maximumImagesCount: 5,
-      width: 500,
-      height: 500,
-      quality: 75
-    }
-
-    ImagePicker.getPictures(options).then(
-      file_uris => this._navCtrl.push(GalleryPage, {images: file_uris}),
-      err => console.log('uh oh')
-    );
-	}
+  constructor(private _navCtrl: NavController) {}
 
 }
 
