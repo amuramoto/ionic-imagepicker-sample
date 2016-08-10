f8acfafc114e55b75a5b77caaba812e8fe65727c
Alex Muramoto <amuramoto@apache.org>
2016-08-10 15:23:16 -0700

    Adds profile view

--- app/pages/gallery/gallery.ts
+++ app/pages/gallery/gallery.ts
@@ -7,31 +7,9 @@ import { NavController, NavParams } from 'ionic-angular';
 export class GalleryPage {
 
 	private images: Array<string>;	
-	private grid: Array<Array<string>>;
   
   constructor(private _navCtrl: NavController, private _navParams: NavParams) {
   	this.images = this._navParams.get('images');
-  	this.grid = Array(Math.ceil(this.images.length/2));
-  }
-
-  ionViewLoaded() {
-  	
-  	let rowNum = 0;
-  	
-  	for (let i = 0; i < this.images.length; i+=2) {
-  		
-      this.grid[rowNum] = Array(2);
-
-  		if (this.images[i]) {
-  			this.grid[rowNum][0] = this.images[i]
-  		}
-  		if (this.images[i+1]) {
-  			this.grid[rowNum][1] = this.images[i+1]
-  		}
-  	
-  		rowNum++;
-  	}
-  		
   }
 
 }
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -15,7 +15,7 @@ export class HomePage {
 
   private openGallery (): void {
     let options = {
-      maximumImagesCount: 8,
+      maximumImagesCount: 15,
       width: 500,
       height: 500,
       quality: 75
