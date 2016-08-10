fc8cbd2bb6614afc6f882e8aed5206d4825e5c04
Alex Muramoto <amuramoto@apache.org>
2016-08-09 17:45:54 -0700

    Adds back getting images from nav params

--- app/pages/gallery/gallery.ts
+++ app/pages/gallery/gallery.ts
@@ -5,8 +5,12 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'build/pages/gallery/gallery.html',
 })
 export class GalleryPage {
+
+	private images: Array<string>;	
+
   
   constructor(private _navCtrl: NavController, private _navParams: NavParams) {
+  	this.images = this._navParams.get('images');
 
   }
 
