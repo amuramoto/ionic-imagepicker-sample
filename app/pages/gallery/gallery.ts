97e44939e1037fdce64b03bf0f0992760dc6bf95
Alex Muramoto <amuramoto@apache.org>
2016-08-09 17:33:05 -0700

    Updates array name to grid for better semantics

--- app/pages/gallery/gallery.ts
+++ app/pages/gallery/gallery.ts
@@ -7,11 +7,11 @@ import { NavController, NavParams } from 'ionic-angular';
 export class GalleryPage {
 
 	private images: Array<string>;	
-	private rows: Array<Array<string>>;
+	private grid: Array<Array<string>>;
   
   constructor(private _navCtrl: NavController, private _navParams: NavParams) {
   	this.images = this._navParams.get('images');
-  	this.rows = Array(Math.ceil(this.images.length/2));
+  	this.grid = Array(Math.ceil(this.images.length/2));
   }
 
   ionViewLoaded() {
@@ -19,12 +19,12 @@ export class GalleryPage {
   	var rowNum = 0;
   	
   	for (var i = 0; i < this.images.length; i+=2) {
-  		this.rows[rowNum] = Array(2);
+  		this.grid[rowNum] = Array(2);
   		if (this.images[i]) {
-  			this.rows[rowNum][0] = this.images[i]
+  			this.grid[rowNum][0] = this.images[i]
   		}
   		if (this.images[i+1]) {
-  			this.rows[rowNum][1] = this.images[i+1]
+  			this.grid[rowNum][1] = this.images[i+1]
   		}
   		
   	
